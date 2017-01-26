a6831e1b7c267b04811c86737a603c17b7d35586
François-Michel De Rainville <f.derainville@gmail.com>
2017-01-26 15:31:59 -0500

    Added necessary panda to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1",
     packages=find_packages(exclude=['examples', 'tests']),
     test_suite="tests",
-    install_requires=["numpy>=1.11", "dataset>=0.7", "filelock>=2.0"],
+    install_requires=["numpy>=1.11", "pandas>=0.19", "dataset>=0.7", "filelock>=2.0"],
     author="François-Michel De Rainville, Olivier Gagnon",
     author_email="chocolate@novasyst.com",
     description="Asynchrone hyperparameter optimization",
