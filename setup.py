0c17a7cfd5c2eca01673a6af82d58d9b8a66482c
François-Michel De Rainville <f.derainville@gmail.com>
2017-03-06 13:50:12 -0500

    Adds scipy and sklearn to setup reqs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1",
     packages=find_packages(exclude=['examples', 'tests']),
     test_suite="tests",
-    install_requires=["numpy>=1.11", "pandas>=0.19", "dataset>=0.8", "filelock>=2.0"],
+    install_requires=["numpy>=1.11", "scipy>=0.18", "scikit-learn>=0.18", "pandas>=0.19", "dataset>=0.8", "filelock>=2.0"],
     author="François-Michel De Rainville, Olivier Gagnon",
     author_email="chocolate@novasyst.com",
     description="Asynchrone hyperparameter optimization",
