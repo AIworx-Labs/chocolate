64b19aa2cd6fc086f5165649f892f84932f13fe3
François-Michel De Rainville <f.derainville@gmail.com>
2017-04-14 20:49:30 -0400

    Added integration with Thompson test

--- tests/test_crossvalidation.py
+++ tests/test_crossvalidation.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
 
+from chocolate.conditional import ThompsonSampling
 from chocolate.space import Space, uniform
 from chocolate.sample import *
 from chocolate.search import *
@@ -111,3 +112,32 @@ class TestSearchRepeat(unittest.TestCase):
 
         token, params = s.next()
         self.assertEqual(token["_chocolate_id"], 1)
+        self.assertEqual(token[repeat_col_name], 2)
+        self.assertEqual(params["a"], 0.6 + (2 - 1))
+
+# TODO: This should be a base SearchAlgorithm test
+class TestRepeatThompson(unittest.TestCase):
+    def setUp(self):
+        self.repeat_col_name = "rep"
+        self.mock_conn = MagicMock(name="connection")
+        self.space = Space({"a": uniform(1, 2)})
+        self.data = [{"a": 0, "_chocolate_id": 0, "_arm_id": 0, self.repeat_col_name: 0, "_loss": 0.3},
+                     {"a": 0, "_chocolate_id": 0, "_arm_id": 0, self.repeat_col_name: 1, "_loss": 0.3},
+                     {"a": 0, "_chocolate_id": 0, "_arm_id": 0, self.repeat_col_name: 2, "_loss": 0.4},
+                     {"a": 0.6, "_chocolate_id": 1, "_arm_id": 0, self.repeat_col_name: 0, "_loss": 0.4},
+                     {"a": 0.6, "_chocolate_id": 1, "_arm_id": 0, self.repeat_col_name: 1, "_loss": 0.5}]
+        
+        self.mock_conn.all_results.return_value = self.data
+        self.mock_conn.count_results = len(self.data)
+        self.mock_conn.pop_id = lambda x: x
+        self.mock_conn.get_space.return_value = self.space
+
+    def test_next(self):
+        cv = Repeat(3, rep_col=self.repeat_col_name)
+        s = ThompsonSampling(CMAES, self.mock_conn, self.space, cv)
+
+        token, params = s.next()
+
+        self.assertEqual(token["_chocolate_id"], 1)
+        self.assertEqual(token[self.repeat_col_name], 2)
+        self.assertEqual(params["a"], 0.6 + (2 - 1))
