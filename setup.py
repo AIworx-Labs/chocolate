cb5f6849a10fdd77bf97bbef8baa7f75b5404e1b
Olivier Gagnon <olivier.gagnon737@gmail.com>
2018-01-26 09:12:08 -0500

    Update setup.py
    Removed ghalton from dependencies. It is not pip installable on windows and only needed for the quasirandom sampler. I'll add it back in the future.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version="0.6",
     packages=find_packages(exclude=['examples', 'tests']),
     test_suite="tests",
-    install_requires=["numpy>=1.11", "scipy>=0.18", "scikit-learn>=0.18", "pandas>=0.19", "dataset>=0.8", "filelock>=2.0", "ghalton>=0.6.1"],
+    install_requires=["numpy>=1.11", "scipy>=0.18", "scikit-learn>=0.18", "pandas>=0.19", "dataset>=0.8", "filelock>=2.0"],
     author="François-Michel De Rainville, Olivier Gagnon",
     author_email="chocolate@novasyst.com",
     description="Asynchrone hyperparameter optimization",
